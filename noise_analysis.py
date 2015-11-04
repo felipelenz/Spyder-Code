Fixed noise_analysis and now all computation is done within t0_noise adn t_max

noise_analysis.py
@@ -36,33 +36,45 @@ def noise_analysis(x,y,fs,t0): #t0 is the pretrigger!
 #    print(t0_noise,tf_noise)
     
     t_max=np.argmax(y[0:t_end]) #because we shift everything to the left using t0=pretrigger, this calculates the peak from 0 to the 3rd ginput point
-    y_topeak=y[0:t_max]
+    y_topeak=y[t0_noise:t_max]
+    plt.plot(x[t0_noise:t_max],y_topeak)
+    show()
     noise_data_window=y[t0_noise:tf_noise]
     sigma=np.std(noise_data_window)
     mean=np.mean(noise_data_window)
 
-    min_ind=np.argmax(np.abs(1.0/((mean+3*sigma)-y_topeak)))   
+    min_ind=np.argmax(np.abs(1.0/((mean+3*sigma)-y_topeak)))
     min_ampl=y_topeak[min_ind]
     max_ampl=np.max(y_topeak)
     y_ampl=max_ampl-min_ampl
 #    print(sigma,min_ampl,max_ampl,y_ampl)
    
-    ten_percent_ind=np.argmax(np.abs(1.0/((0.1*y_ampl+min_ampl)-y_topeak)))  
-    twenty_percent_ind=np.argmax(np.abs(1.0/((0.2*y_ampl+min_ampl)-y_topeak))) 
-    fifty_percent_ind=np.argmax(np.abs(1.0/((0.5*y_ampl+min_ampl)-y_topeak))) 
-    eighty_percent_ind=np.argmax(np.abs(1.0/((0.8*y_ampl+min_ampl)-y_topeak))) 
-    ninety_percent_ind=np.argmax(np.abs(1.0/((0.9*y_ampl+min_ampl)-y_topeak))) 
-    risetime_10_90=(ninety_percent_ind-ten_percent_ind)/fs
+    ten_percent_ind=np.argmax(np.abs(1.0/((0.1*y_ampl+min_ampl)-y_topeak)))
+    twenty_percent_ind=np.argmax(np.abs(1.0/((0.2*y_ampl+min_ampl)-y_topeak)))
+    fifty_percent_ind=np.argmax(np.abs(1.0/((0.5*y_ampl+min_ampl)-y_topeak)))
+    eighty_percent_ind=np.argmax(np.abs(1.0/((0.8*y_ampl+min_ampl)-y_topeak)))
+    ninety_percent_ind=np.argmax(np.abs(1.0/((0.9*y_ampl+min_ampl)-y_topeak)))
     
-#    print("10",ten_percent_ind)    
+#    print("min",min_ind)  
+#    print("10",ten_percent_ind)  
+#    print("90",ninety_percent_ind)
     
 #    print("standard deviation= %r noise mean= %r"%(sigma,mean))
-    plot(x,y, 'b', \
+    plot(x[t0_noise:t_max],y_topeak, 'b', \
     x[t0_noise:tf_noise],y[t0_noise:tf_noise], 'g', \
-    [x[0],x[-1]],[mean,mean], 'r', \
-    [x[0],x[-1]],[mean+sigma,mean+sigma], '--r', \
-    [x[0],x[-1]],[mean-sigma,mean-sigma], '--r', \
-    [x[min_ind],x[ten_percent_ind],x[ninety_percent_ind]],[y[min_ind],y[ten_percent_ind],y[ninety_percent_ind]], 'or')
+    [x[t0_noise],x[t_max]],[mean,mean], 'r', \
+    [x[t0_noise],x[t_max]],[mean+sigma,mean+sigma], '--r', \
+    [x[t0_noise],x[t_max]],[mean-sigma,mean-sigma], '--r', \
+    [x[min_ind]+t0_noise*sampling_time,x[ten_percent_ind]+t0_noise*sampling_time,x[ninety_percent_ind]+t0_noise*sampling_time],[y_topeak[min_ind],y_topeak[ten_percent_ind],y_topeak[ninety_percent_ind]], 'or')
     show()
     
-    return ten_percent_ind,twenty_percent_ind,fifty_percent_ind,eighty_percent_ind,ninety_percent_ind,risetime_10_90
+    ten_percent_time=(ten_percent_ind+t0_noise)*sampling_time
+    twenty_percent_time=(twenty_percent_ind+t0_noise)*sampling_time
+    fifty_percent_time=(fifty_percent_ind+t0_noise)*sampling_time
+    eighty_percent_time=(eighty_percent_ind+t0_noise)*sampling_time
+    ninety_percent_time=(ninety_percent_ind+t0_noise)*sampling_time
+    risetime_10_90=(ninety_percent_time-ten_percent_time)
+    
+    print("10%% time",ten_percent_time)
+    print("90%% time",twenty_percent_time)
+    return ten_percent_time,twenty_percent_time,fifty_percent_time,eighty_percent_time,ninety_percent_time,risetime_10_90