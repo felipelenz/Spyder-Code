01ada634d9239b2cfb0c4ca2fa76d30befa8975c
Felipe Lenz <felipelenzc@gmail.com>
2016-07-20 11:27:38 -0400

    modify this instead of the root function RS_plot_2016.py

---
+++ call_plot_function.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 20 11:21:44 2016
+
+@author: lenz
+"""
+
+from RS_plot_2016 import RS_plot_2016
+from RS_plot_2016_diodes14 import RS_plot_2016_diodes14
+from RS_plot_2016_diodes15 import RS_plot_2016_diodes15
+
+RS_plot_2016(71516,4,0,1,0)
+RS_plot_2016_diodes14(71516,4,0,1,0)
+RS_plot_2016_diodes15(71516,4,0,1,0)
